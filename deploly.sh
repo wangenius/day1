b9e56718086f2d61a2bdb33e10f020e4075b0279
jh-zhang21 <jh-zhang21@tsinghua.org.cn>
2025-08-13 00:15:57 +0800

    optimize deploy script

--- deploly.sh
+++ deploly.sh
@@ -4,6 +4,7 @@
 _DEPLOY_DIR="/data/web"
 _REPO="startup-mod"
 _FRONT_DIR="${_REPO}/client"
+_BACK_DIR="${_REPO}/server"
 _BASEDIR=$(dirname $0)
 pushd ${_BASEDIR} > /dev/null
 
@@ -17,6 +18,12 @@ mkdir -p ${_DEPLOY_DIR}
 rm -rf ${_DEPLOY_DIR}/*
 rsync -avtP dist/ ${_DEPLOY_DIR}
 systemctl restart nginx.service
+
+# deploy back
+cd
+cd "${_BACK_DIR}"
+git pull origin main
+python src/app.py
 
 # recovery
 popd > /dev/null
